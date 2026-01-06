0715809a268ae5bd7fd320c98efd9bd34645f931
curly-coder <brat.thor8@gmail.com>
2026-01-06 15:14:51 +0200

    add icon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,9 +29,6 @@ export const About = () => {
                         <a href="#contact" className="cosmic-button">
                             Get in Touch
                         </a>
-                         <a href="#contact" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 duration-300">
-                            Download CV
-                        </a>
                     </div>
                 </div>
                 <div className="grid grid-cols-1 gap-6">
